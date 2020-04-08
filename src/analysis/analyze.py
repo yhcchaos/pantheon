9e20a6a3449952945c517905b160a15912fb52c6
Olivier Delalleau <507137+odelalleau@users.noreply.github.com>
2020-04-08 07:49:27 -0700

    Explicitly call python2 in analyze script
    This is because with a regular setup, `python` will default to Python 3 (which
    may not have matplotlib installed).

--- src/analysis/analyze.py
+++ src/analysis/analyze.py
@@ -14,8 +14,8 @@ def main():
     plot = path.join(analysis_dir, 'plot.py')
     report = path.join(analysis_dir, 'report.py')
 
-    plot_cmd = ['python', plot]
-    report_cmd = ['python', report]
+    plot_cmd = ['python2', plot]
+    report_cmd = ['python2', report]
 
     for cmd in [plot_cmd, report_cmd]:
         if args.data_dir:
