8de398316a7d5d454d7a0a47da62b9a2609aac39
Olivier Delalleau <507137+odelalleau@users.noreply.github.com>
2020-04-24 13:26:40 -0700

    Add new scheme mvfst_rl_fixed (fixed cwnd)
    Like mvfst_rl_random, this is a baseline scheme for testing purpose.

---
+++ src/wrappers/mvfst_rl_fixed.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python2
+
+# Policy that aims at reaching a fixed cwnd
+
+import os
+from os import path
+import sys
+import string
+import shutil
+import time
+from subprocess import check_call, call, Popen, PIPE
+
+import arg_parser
+import context
+from helpers import utils
+
+from mvfst_rl import setup_mvfst, dependencies_mvfst
+
+def main():
+    args = arg_parser.sender_first()
+
+    cc_repo = path.join(context.third_party_dir, 'mvfst-rl')
+    src = path.join(cc_repo, '_build/build/traffic_gen/traffic_gen')
+
+    if args.option == 'deps':
+        dependencies_mvfst()
+        return
+
+    if args.option == 'setup':
+        setup_mvfst(cc_repo)
+        return
+
+    if args.option == 'sender':
+        cmd = [
+            src,
+            '--mode=server',
+            '--host=0.0.0.0',  # Server listens on 0.0.0.0
+            '--port=%s' % args.port,
+            '--cc_algo=rl',
+        ] + args.extra_args.split() + [
+            # extra_args might have --cc_env_mode already, so we set this
+            # at the end to override.
+            '--cc_env_mode=fixed',
+        ]
+        check_call(cmd)
+        return
+
+    # We use cubic for the client side to keep things simple. It doesn't matter
+    # here as we are simulating server-to-client flow, and the client simply
+    # sends a hello message to kick things off.
+    if args.option == 'receiver':
+        cmd = [
+            src,
+            '--mode=client',
+            '--host=%s' % args.ip,
+            '--port=%s' % args.port,
+            '--cc_algo=cubic',
+        ]
+        check_call(cmd)
+        return
+
+
+if __name__ == '__main__':
+    main()
